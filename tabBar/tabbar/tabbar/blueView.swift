1c7ebca94b3ce9d0c7099ff1fc9fc809f4f1dce6
kwilliam777 <66508487+kwilliam777@users.noreply.github.com>
2021-05-16 23:43:48 +0900

    tabBar study

---
+++ tabBar/tabbar/tabbar/blueView.swift
@@ -0,0 +1,15 @@
+//
+//  blueView.swift
+//  tabbar
+//
+//  Created by 김응진 on 2021/05/16.
+//
+
+import UIKit
+
+class blueView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+}
---
+++ tabBar/tabbar/tabbar/grayView.swift
@@ -0,0 +1,15 @@
+//
+//  grayView.swift
+//  tabbar
+//
+//  Created by 김응진 on 2021/05/16.
+//
+
+import UIKit
+
+class grayView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .gray
+    }
+}
